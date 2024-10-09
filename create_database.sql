e7e632c716028cc73dae16f6d66ed79ecc7ff400
fallousman <ouzeulass97@gmail.com>
2024-10-09 07:49:46 +0100

    Supprimer le fichier create_database.sql

--- create_database.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE courses (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    level ENUM('Débutant', 'Intermédiaire', 'Avancé') NOT NULL
-);
